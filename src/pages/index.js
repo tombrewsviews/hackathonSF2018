aff90cf5012dc42e5b9d5de3eb3b727385acde61
Tom Parandyk <tom@uxtemple.com>
2018-08-28 00:18:56 +0200

    initial

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi everyone</h1>
+    <p>My name is Tom</p>
+    <p>I design stuff for fun</p>
+    <p>I love hands-on approach to development</p>
+    <p>Looking forawrd meting you all</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Excited</h1>
+    <p style={{ fontSize: '50px' }}>🤩</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
